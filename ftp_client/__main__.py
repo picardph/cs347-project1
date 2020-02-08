90ec1405cd33544f096fc6b322b1ceb0a7331b95
Zac Laymon <laymonz@mail.gvsu.edu>
2020-02-08 11:31:59 -0500

    Added default values for IP and port as well as basic connection.

--- ftp_client/__main__.py
+++ ftp_client/__main__.py
@@ -1,7 +1,14 @@
 import socket
 
-def Connect(ip, soc):
-    pass
+soc = None
+ip = '127.0.0.1'
+port = 3927
+
+def Connect(ip=ip, port=port):
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.connect(ip, port)
+    print("Connected to " + str(ip) + ":" + port)
+
 
 def List():
     pass
